ff9d9a6545c6e2c95c3bea9cf76ac3275be73375
katrinawoods <75281195+katrinawoods@users.noreply.github.com>
2023-09-02 20:30:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 const correctOrder = ["The", "library", "opened", "in", "the", "year", "2018"];
 let draggedItem = null;
+let pickedUpItem = null;
 
-// Shuffle function for initial load
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+        [array[i], array[j]] = [array[j], array[i]]; 
     }
     return array;
 }
 
-// Shuffle the words on initial load
 const shuffledOrder = shuffleArray([...correctOrder]);
 shuffledOrder.forEach((word, index) => {
     document.getElementById(`word${index + 1}`).textContent = word;
 });
-
-// Drag and Drop logic
 
 document.querySelectorAll('.draggable').forEach(draggable => {
     draggable.addEventListener('dragstart', e => {
@@ -51,7 +48,6 @@ document.getElementById("checkAnswerButton").addEventListener('click', () => {
     checkOrder();
 });
 
-// Check the order of words
 function checkOrder() {
     let currentOrder = Array.from(document.querySelectorAll('.dropzone .draggable')).map(item => item.textContent);
     currentOrder.forEach((word, index) => {
@@ -63,12 +59,26 @@ function checkOrder() {
     });
 }
 
-// Keyboard navigation
 document.querySelectorAll('.draggable').forEach((item, idx, array) => {
     item.addEventListener('keydown', e => {
-        if (e.key === 'ArrowRight' && array[idx + 1]) {
+        if (e.key === 'Space' && !pickedUpItem) {
+            pickedUpItem = item;
+            item.style.backgroundColor = 'lightblue'; 
+            e.preventDefault();
+        } else if (e.key === 'Space' && pickedUpItem) {
+            if (pickedUpItem.parentNode.className === 'dropzone') {
+                if (e.target.parentNode.nextElementSibling) {
+                    e.target.parentNode.nextElementSibling.append(pickedUpItem);
+                } else if (e.target.parentNode.previousElementSibling) {
+                    e.target.parentNode.previousElementSibling.append(pickedUpItem);
+                }
+            }
+            pickedUpItem.style.backgroundColor = ''; 
+            pickedUpItem = null;
+            e.preventDefault();
+        } else if (e.key === 'ArrowRight' && !pickedUpItem && array[idx + 1]) {
             array[idx + 1].focus();
-        } else if (e.key === 'ArrowLeft' && array[idx - 1]) {
+        } else if (e.key === 'ArrowLeft' && !pickedUpItem && array[idx - 1]) {
             array[idx - 1].focus();
         }
     });
