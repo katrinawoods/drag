745104d6b5371f241c19728b32a11ccfa7f6a316
katrinawoods <75281195+katrinawoods@users.noreply.github.com>
2023-09-02 20:44:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const correctOrder = ["The", "library", "opened", "in", "the", "year", "2018"];
 let selectedItem = null;
 
-// ... [Shuffle function remains the same]
+document.addEventListener("DOMContentLoaded", function(event) {
+    // Shuffle the words on initial load
+    const shuffledOrder = shuffleArray([...correctOrder]);
+    shuffledOrder.forEach((word, index) => {
+        document.getElementById(`word${index + 1}`).textContent = word;
+    });
+});
 
-// ... [Shuffling logic remains the same]
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+}
 
 // Keyboard navigation
 document.querySelectorAll('.draggable').forEach((item, idx, array) => {
@@ -39,4 +51,49 @@ document.querySelectorAll('.draggable').forEach((item, idx, array) => {
     });
 });
 
-// Existing drag and drop logic remains unchanged.
+// Drag and Drop logic
+let draggedItem = null;
+
+document.querySelectorAll('.draggable').forEach(draggable => {
+    draggable.addEventListener('dragstart', e => {
+        draggedItem = draggable;
+        setTimeout(() => {
+            draggable.style.display = 'none';
+        }, 0);
+    });
+
+    draggable.addEventListener('dragend', () => {
+        setTimeout(() => {
+            draggedItem.style.display = '';
+            draggedItem = null;
+        }, 0);
+    });
+
+    document.querySelectorAll('.dropzone').forEach(dropzone => {
+        dropzone.addEventListener('dragover', e => {
+            e.preventDefault();
+        });
+
+        dropzone.addEventListener('drop', e => {
+            e.preventDefault();
+            if (!dropzone.querySelector('.draggable')) {
+                dropzone.append(draggedItem);
+            }
+        });
+    });
+});
+
+document.getElementById("checkAnswerButton").addEventListener('click', () => {
+    checkOrder();
+});
+
+function checkOrder() {
+    let currentOrder = Array.from(document.querySelectorAll('.dropzone .draggable')).map(item => item.textContent);
+    currentOrder.forEach((word, index) => {
+        if (word === correctOrder[index]) {
+            document.getElementById(`word${index + 1}`).style.backgroundColor = 'lightgreen';
+        } else {
+            document.getElementById(`word${index + 1}`).style.backgroundColor = 'lightcoral';
+        }
+    });
+}
